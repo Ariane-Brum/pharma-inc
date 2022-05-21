c479572b216f647a3bf114ea69d320eacbe261be
Ariane-Brum <arianebrum27@gmail.com>
2022-05-21 13:23:22 -0300

    :wrench: Add App, favicon, logo and main

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Header } from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
