5743be365416b3895d0a0dcd9b6540158444951e
Ariane-Brum <arianebrum27@gmail.com>
2022-05-21 14:03:05 -0300

    :sparkles: Add style of the PersonalInfo component

---
+++ src/components/PersonalInfo/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const PersonalInfo = styled.div`
+  padding: 1rem;
+  box-shadow: 0 2px 2px 0 rgb(0 0 0 / 30%);
+  flex: 1;
+  border-radius: 5px;
+  background: var(--white);
+`;
