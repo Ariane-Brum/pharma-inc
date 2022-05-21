eabf9c580fe3d29ff5b14572618eceb89e9bb39c
Ariane-Brum <arianebrum27@gmail.com>
2022-05-21 13:37:31 -0300

    :sparkles: Create helper component

---
+++ src/helpers/Loading/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { CircleNotch } from "phosphor-react";
+import * as S from "./styles";
+export const Loading = () => {
+  return (
+    <S.Loading>
+      <CircleNotch size={40} color="#00453A" />
+    </S.Loading>
+  );
+};
