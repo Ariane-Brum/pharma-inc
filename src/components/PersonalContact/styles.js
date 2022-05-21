5b9ea6cc520ce8f3e649ced0326433d09ecbcbc3
Ariane-Brum <arianebrum27@gmail.com>
2022-05-21 14:00:18 -0300

    :sparkles: Add style of the PersonalContact component

---
+++ src/components/PersonalContact/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ContactInfo = styled.div`
+  padding: 1rem;
+  box-shadow: 0 2px 2px 0 rgb(0 0 0 / 30%);
+  background: var(--white);
+  border-radius: 5px;
+  flex: 1;
+`;
